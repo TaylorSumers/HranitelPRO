d369b3a72d8d9c56de353144b8aa8d4e03ac58c8
TaylorSumers <suvorint@gmail.com>
2023-04-03 14:45:01 +0300

    Added front to PersonalVisitPage

--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Client.Pages;
 
 namespace Client
 {
@@ -23,6 +24,7 @@ namespace Client
         public MainWindow()
         {
             InitializeComponent();
+            MainFrame.Content = new PersonalVisitPage();
         }
     }
 }
